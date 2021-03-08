05ba27860cb7da496c870d18170999d995dcf99c
Kay0s <hamilton.kristina@gmail.com>
2021-03-07 19:57:23 -0600

    added models/Appointment.js

--- models/User.js
+++ models/User.js
@@ -2,26 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
-    loyaltyLevel: {
-        type: String,
-        required: true,
-    },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  loyaltyLevel: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model("users", UserSchema);
