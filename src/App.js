1a2bdee71ddaa8dcacecf539eeec1b4501d88bd1
Dj Meechlouch <Meechlouch@gmail.com>
2021-03-09 19:33:30 -0600

    new styling login page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import Login from "./components/Login/Login";
+import Login from "./pages/Login/Login";
 import Logout from "./components/Logout/Logout";
 import Input from "./components/Input/Input";
 
-require("dotenv").config();
-
 function App() {
-  return (
-    <section className="App h-screen w-full flex justify-center items-center bg-green-500">
-      <div className="w-full max-w-md bg-gray-800">
-        <form action="" className=" bg-white shadow-md rounded px-8 py-8 pt-8">
-          <Input
-            type="email"
-            name="email"
-            id=""
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
-            placeholder="Johnbull@example.com"
-          />
-          <Input
-            type="password"
-            name="email"
-            id=""
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300"
-            placeholder="Enter your password"
-          />
-          <Login />
-          <Logout />
-        </form>
-      </div>
-    </section>
-  );
+  return <Login />;
 }
 export default App;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { GoogleLogin } from "react-google-login";
+import { refreshTokenSetup } from "../../utils/refreshToken";
+
+function Login() {
+  const googleSuccess = async (res) => {
+    const result = res?.profileObj;
+    console.log("Login Success: currentUser:", res.profileObj);
+    alert(`Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`);
+
+    refreshTokenSetup(res);
+  };
+
+  const googleFailure = (res) => {
+    console.log("Login failed: res:", res);
+    alert("Failed to login. 😢");
+  };
+
+  return (
+    <section className="App h-screen w-full flex justify-center items-center bg-green-500">
+      <div className="w-full max-w-md bg-gray-800">
+        <div className="text-center bg-white shadow-md rounded px-8 py-8 pt-8">
+          <h3>Sudz-Time</h3>
+          <hr></hr>
+          <p className="m-10">For Scheduling your Car Wash on the Run</p>
+          <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={googleSuccess}
+            onFailure={googleFailure}
+            cookiePolicy={"single_host_origin"}
+            style={{ marginTop: "100px" }}
+            isSignedIn={true}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Login;
