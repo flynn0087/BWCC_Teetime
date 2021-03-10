f72a68b5278ebaac3b355cd0a294bc41a08b9e20
nate <rotarygurter@gmail.com>
2021-03-09 19:58:11 -0600

    qr code

---
+++ src/components/success/Success.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Success() {
+  return (
+    <div>
+      <img src="https://api.qrserver.com/v1/create-qr-code/?data=Nevergonnagiveyouuporletyoudown&size=200x200" />
+    </div>
+  );
+}
+
+export default Success;
