ca8803736f2d7ecaeb97eaddd2c20851353fa05a
Ben Flynn <flynn0087@gmail.com>
2021-03-11 17:43:00 -0600

    got the image centered

--- client/src/pages/SuccessQR.js
+++ client/src/pages/SuccessQR.js
@@ -4,22 +4,22 @@ import React from "react";
 function SuccessQR() {
   return (
     <div >
-      <div class="md:container md:mx-auto">
+      <div className="container mx-auto">
         <div className="flow-root bg-gray-300">
-          <p className="text-center">SUCCESS AND CONFIRMATION</p>
+          <p className="text-center my-10">SUCCESS AND CONFIRMATION</p>
       
-          <p className="text-center">You've scheduled a carwash at EXAMPLETIME at EXAMPLELOCATION</p>
+          <p className="text-center my-10">You've scheduled a carwash at EXAMPLETIME at EXAMPLELOCATION</p>
         </div>
       </div>
 
-      <div class="flow-root bg-red-100">
-        <img className="object-center" alt="qr code" src="https://via.placeholder.com/350"></img>
+      <div className="container bg-red-100">
+        <img className="mx-auto my-auto" alt="qr code" src="https://via.placeholder.com/350"></img>
       </div>
 
-      <div class="flow-root bg-green-100">
-        <p className="text-center" >Use this QR code to claim your car wash appointment in the reserved line</p>
+      <div className="container flow-root bg-green-100">
+        <p className="text-center mt-10 mb-6" >Use this QR code to claim your car wash appointment in the reserved line</p>
         <br></br>
-        <p className="text-center" >Refer your friends to the Suds Time App!</p>
+        <p className="text-center my-6 mb-10" >Refer your friends to the Suds Time App!</p>
       </div>
     </div>
   );
