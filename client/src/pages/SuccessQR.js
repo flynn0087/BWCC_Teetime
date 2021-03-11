52dcffeaf4f127217dfccc54357eee215f8fd51b
Ben Flynn <flynn0087@gmail.com>
2021-03-11 17:00:52 -0600

    layout generated

--- client/src/pages/SuccessQR.js
+++ client/src/pages/SuccessQR.js
@@ -3,8 +3,24 @@ import React from "react";
 
 function SuccessQR() {
   return (
-    <div>
-      <p>Hello Success-QR Page!</p>
+    <div >
+      <div class="md:container md:mx-auto">
+        <div className="flow-root bg-gray-300">
+          <p className="text-center">SUCCESS AND CONFIRMATION</p>
+      
+          <p className="text-center">You've scheduled a carwash at EXAMPLETIME at EXAMPLELOCATION</p>
+        </div>
+      </div>
+
+      <div class="flow-root bg-red-100">
+        <img className="object-center" alt="qr code" src="https://via.placeholder.com/350"></img>
+      </div>
+
+      <div class="flow-root bg-green-100">
+        <p className="text-center" >Use this QR code to claim your car wash appointment in the reserved line</p>
+        <br></br>
+        <p className="text-center" >Refer your friends to the Suds Time App!</p>
+      </div>
     </div>
   );
 }
