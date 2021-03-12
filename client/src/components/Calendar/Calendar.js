0c1421a0a897d7b5984f9aac3f9727e9aaa06e4b
Kay0s <hamilton.kristina@gmail.com>
2021-03-11 18:55:52 -0600

    added Calendar.js component

---
+++ client/src/components/Calendar/Calendar.js
@@ -0,0 +1,97 @@
+import React from 'react';
+require("dotenv").config();
+
+function App() {
+
+    let gapi = window.gapi
+ 
+    let CLIENT_ID = `${CLIENTID}`
+    let API_KEY = `${APIKEY}`
+    let DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
+    let SCOPES = "https://www.googleapis.com/auth/calendar.events"
+  
+    const handleClick = () => {
+      gapi.load("client:auth2", () => {
+        console.log("loaded client")
+  
+        gapi.client.init({
+          apiKey: API_KEY,
+          clientId: CLIENT_ID,
+          discoveryDocs: DISCOVERY_DOCS,
+          scope: SCOPES,
+        })
+  
+        gapi.client.load("calendar", "v3", () => console.log("load calendar"))
+  
+        gapi.auth2.getAuthInstance().signIn()
+        .then(() => {
+          
+          let event = {
+            "id": "",
+            "status": "",
+            "htmlLink": "",
+            "summary": "",
+            "description": "",
+            "location": "",
+
+            "start": {
+              "date": (new Date()).toISOString(),
+              "dateTime": valueOf(),
+              "timeZone": "America/Minneapolis"
+            },
+            "end": {
+              "date": (new Date()).toISOString(),
+              "dateTime": "America/Minneapolis",
+              "timeZone": ""
+            },
+            "attendees": [
+                {
+                  "id": "",
+                  "email": "",
+                  "displayName": "",
+                }
+              ],
+          }
+  
+          let request = gapi.client.calendar.events.insert({
+            "calendarId": "primary",
+            "resource": event,
+          })
+  
+          request.execute(event => {
+            console.log(event)
+            window.open(event.htmlLink)
+          })
+          
+  
+          // get events
+          gapi.client.calendar.events.list({
+            "calendarId": "primary",
+            "timeMin": (new Date()).toISOString(),
+            "showDeleted": false,
+            "singleEvents": true,
+            "maxResults": 5,
+            "orderBy": "startTime"
+          }).then(response => {
+            const events = response.result.items
+            console.log("EVENTS: ", events)
+          })
+         
+      
+  
+        })
+      })
+    }
+  
+  
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Add to Google Calendar</p>
+          <button onClick={handleClick}>Add Event</button>
+        </header>
+      </div>
+    );
+  }
+  
+  export default App;
