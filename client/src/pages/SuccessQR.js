e9f9f78600a587cab6ce137a9389bc8b1b41c6d2
nate <rotarygurter@gmail.com>
2021-03-12 12:03:19 -0600

    updated file

--- client/src/pages/SuccessQR.js
+++ client/src/pages/SuccessQR.js
@@ -9,7 +9,9 @@ function SuccessQR() {
         <div className="flow-root bg-gray-300">
           <p className="text-center my-10">SUCCESS AND CONFIRMATION</p>
 
-          <p className="text-center my-10">You've scheduled a carwash at EXAMPLETIME at EXAMPLELOCATION</p>
+          <p className="text-center my-10">
+            You've scheduled a carwash at EXAMPLETIME at EXAMPLELOCATION
+          </p>
         </div>
       </div>
 
@@ -19,10 +21,13 @@ function SuccessQR() {
 
       <div className="container flow-root bg-green-100">
         <p className="text-center mt-10 mb-6">
-          Use this QR code to claim your car wash appointment in the reserved line
+          Use this QR code to claim your car wash appointment in the reserved
+          line
         </p>
         <br></br>
-        <p className="text-center my-6 mb-10">Refer your friends to the Suds Time App!</p>
+        <p className="text-center my-6 mb-10">
+          Refer your friends to the Suds Time App!
+        </p>
       </div>
     </div>
   );
