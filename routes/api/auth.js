94b2ea3cc5ac1d73b7e020cb2498da80bda5583b
Dj Meechlouch <Meechlouch@gmail.com>
2021-03-13 15:02:55 -0600

    linting fixed

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../../models/User");
 const { googlelogin } = require("../../controllers/authController");
 router.get("/", (req, res) => {
   res.send("hello");
