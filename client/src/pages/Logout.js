0986ae432fc7f4e917cd6cd1ba53f0da8e459abf
Ben Flynn <flynn0087@gmail.com>
2021-03-13 12:06:20 -0600

    container and text added to logout

--- client/src/pages/Logout.js
+++ client/src/pages/Logout.js
@@ -1,2 +1,17 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+
+function Logout () {
+   return (
+       <div>
+           <Navbar />
+           <div className="container mx-auto bg-green-500">
+               <p className="text-center text-white">
+                   You have sucessfully logged out!
+               </p>
+           </div>
+       </div>
+   );
+} 
+
+export default Logout;
