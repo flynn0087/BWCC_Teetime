119cbd3930ebf36a1db8ba9aa942f61806c1303f
Ben Flynn <flynn0087@gmail.com>
2021-03-13 12:23:21 -0600

    adjusted layout

--- client/src/pages/Logout.js
+++ client/src/pages/Logout.js
@@ -5,10 +5,16 @@ function LogoutPage () {
    return (
        <div>
            <Navbar />
-           <div className="container mx-auto bg-green-500">
-               <p className="text-center text-white">
-                   You have sucessfully logged out!
+           <div className="container mx-auto">
+               <div className="flow-root bg-green-500 h-screen">
+                <p className="mt-20 text-center text-white text-2xl">
+                   You have successfully logged out!
                </p>
+               <br></br>
+               <p className="mt-40 text-center text-white text-2xl">
+                   Click this link to return to login page!
+               </p>
+               </div>
            </div>
        </div>
    );
--- client/src/pages/SuccessQR.js
+++ client/src/pages/SuccessQR.js
@@ -9,9 +9,9 @@ function SuccessQR() {
       <Navbar />
       <div className="container mx-auto">
         <div className="flow-root bg-green-500">
-          <p className="text-center my-10 text-yellow-300">SUCCESS AND CONFIRMATION</p>
+          <p className="text-center my-10 text-yellow-300 text-4xl">SUCCESS AND CONFIRMATION</p>
 
-          <p className="text-center text-white my-10">
+          <p className="text-center text-white my-10 text-2xl">
             You've scheduled a carwash at EXAMPLETIME at EXAMPLELOCATION
           </p>
         </div>
@@ -22,12 +22,12 @@ function SuccessQR() {
       </div>
 
       <div className="container flow-root bg-green-500 h-screen">
-        <p className="text-center text-white mt-10 mb-6">
+        <p className="text-center text-white mt-10 mb-6 text-2xl">
           Use this QR code to claim your car wash appointment in the reserved
           line
         </p>
         <br></br>
-        <p className="text-center text-white my-6 mb-10">
+        <p className="text-center text-white my-6 mb-10 text-2xl">
           Refer your friends to the Suds Time App!
         </p>
       </div>
