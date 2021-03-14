becf11d2076c4b489d9823068ea6d266d893689b
Kay0s <hamilton.kristina@gmail.com>
2021-03-13 21:40:02 -0600

    added context provider

--- client/src/privateRoute/PrivateRoute.js
+++ client/src/privateRoute/PrivateRoute.js
@@ -1,19 +1,22 @@
-import React from "react";
-import { Redirect, Route, useLocation } from "react-router-dom";
-import { fakeAuth } from "../components/Login/Login";
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { AuthContext } from "../../../routes/api/auth";
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  const location = useLocation();
-
+const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
+  const {currentUser} = useContext(AuthContext);
   return (
-    <Route {...rest}>
-      {fakeAuth.isAuthenticated === true ?
-        <Component />
-      :
-        <Redirect to={{ pathname: "/login", state: { from: location } }} />
+    <Route
+      {...rest}
+      render={routeProps =>
+        !!currentUser ? (
+          <RouteComponent {...routeProps} />
+        ) : (
+          <Redirect to={"/login"} />
+        )
       }
-    </Route>
+    />
   );
 };
 
-export default PrivateRoute;
+
+export default PrivateRoute
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const { googlelogin } = require("../../controllers/authController");
+import React, { useEffect, useState } from "react";
+export const AuthContext = React.createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [pending, setPending] = useState(true);
+
+  useEffect(() => {
+    app.auth().onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setPending(false);
+    });
+  }, []);
+
+  if(pending){
+    return <>Loading...</>;
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        currentUser
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
 router.get("/", (req, res) => {
   res.send("hello");
 });
