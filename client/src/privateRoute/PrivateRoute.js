664261f55b6ab5b1f3df8cf0c125eafa295207f0
Kay0s <hamilton.kristina@gmail.com>
2021-03-13 21:19:08 -0600

    updated file pathway on PrivateRoute.js

--- client/src/privateRoute/PrivateRoute.js
+++ client/src/privateRoute/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Redirect, Route, useLocation } from "react-router-dom";
-import { fakeAuth } from "../components/Login";
+import { fakeAuth } from "../components/Login/Login";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const location = useLocation();
