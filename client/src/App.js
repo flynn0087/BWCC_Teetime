c812fab7b8bae9811433415d5d7902d201ad0973
Kay0s <hamilton.kristina@gmail.com>
2021-03-13 20:52:51 -0600

    added PrivateRoute components to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import AvailWash from "./pages/AvailWash";
 import Calendar from "./pages/Calendar";
 import SuccessQR from "./pages/SuccessQR";
 import Login from "./pages/Login";
 import LogoutPage from "./pages/Logout";
+import PrivateRoute from "./privateRoute/PrivateRoute";
 
 function App() {
   return (
     <div>
       <Router>
+        <Switch>
         <Route exact path="/" component={Login} />
         <Route path="/oauth" component={Login} />
         <Route path="/home" component={Home} />
         <Route path="/availwash" component={AvailWash} />
-        <Route path="/calendar" component={Calendar} />
-        <Route path="/successqr" component={SuccessQR} />
-        <Route path="/logout" component={LogoutPage} />
+        <PrivateRoute path="/calendar" component={Calendar} />
+        <PrivateRoute path ="/successqr" component={SuccessQR} />
+        <PrivateRoute path="/logout" component={LogoutPage} />
+        </Switch>
       </Router>
     </div>
   );
