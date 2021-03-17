7a2870a4357ce22799a1be51ac58f80f14f00d09
Dj Meechlouch <Meechlouch@gmail.com>
2021-03-16 21:03:33 -0500

    server fix

--- server.js
+++ server.js
@@ -25,19 +25,15 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-);
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-
-
 
 // Add routes, both API and view
 app.use(routes);
 
 mongoose
-  .connect(process.env.MONGODB_URI || `mongodb://${process.env.HOST}/${process.env.DB_NAME}`, {
+  .connect(process.env.MONGO_URI || `mongodb://${process.env.HOST}/${process.env.DB_NAME}`, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
