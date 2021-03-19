58abe83b533732d51c42b44c13adffd57a4edecc
Kay0s <hamilton.kristina@gmail.com>
2021-03-19 13:51:59 -0500

    npm i to add updates from merge on main

--- routes/api/events.js
+++ routes/api/events.js
@@ -1,22 +1,10 @@
 const router = require("express").Router();
-const db = require("../../models");
-const passport = require("../../config/passport");
 
 // Matches with "/api/events"
 router.route("/")
   .get(eventsController.findAll)
   .post(eventsController.create);
 
-router.get("/events", passport.authenticate("local"), (req, res) => {
-  db.Event.find({})
-    // .populate("events")
-    .the(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
 
 // Matches with "/api/events/:id"
 router
