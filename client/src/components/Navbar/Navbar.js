e83120594a9b693cb62173327751017b49472d97
Dj Meechlouch <Meechlouch@gmail.com>
2021-03-20 00:57:52 -0500

    styled navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,37 +1,33 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+
 import { Link } from "react-router-dom";
 import Logout from "../Logout/Logout";
+import React, { useContext } from "react";
+import LoginContext from "../../utils/LoginContext";
 
 function Navbar() {
+  const { isLoggedIn } = useContext(LoginContext);
   return (
-    <div>
-      <nav className="bg-gray-900">
-        <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <nav className="flex items-center justify-between h-16">
-            <nav className="flex items-center">
-              <nav>
-                <nav className="ml-10 flex items-baseline space-x-4">
-                  <Link to="/calendar"
-                    className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Tee Time Calendar
-                  </Link>
-                </nav>
-                <nav className="ml-10 flex items-baseline space-x-4">
-                  <Link to="/successQR"
-                    className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                   Success QR
-                  </Link>
-                </nav>
-              </nav>
-              <Logout />
-            </nav>
-          </nav>
-        </nav>
-      </nav>
-    </div>
+    <nav className="bg-gray-900 py-2 flex flex-row">
+      <Link to="/home" className="pl-4">
+        <img className="rounded-full" src={isLoggedIn.headshot} alt="headshot"></img>
+      </Link>
+      <div className="ml-10 flex items-center space-x-4 justify-end">
+        <Link
+          to="/calendar"
+          className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+        >
+          Tee Time Calendar
+        </Link>
+        <Link
+          to="/successQR"
+          className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+        >
+          Success QR
+        </Link>
+        <Logout />
+      </div>
+    </nav>
   );
 }
 
