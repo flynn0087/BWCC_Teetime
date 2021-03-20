b3286ad0926837fe762213425eaa9e4a8e7ddb74
unknown <rotarygurter@gmail.com>
2021-03-20 11:31:37 -0500

    updated styling and added home button

--- client/src/pages/SuccessQR.js
+++ client/src/pages/SuccessQR.js
@@ -6,29 +6,28 @@ import Navbar from "../components/Navbar/Navbar";
 
 function SuccessQR(  ) {
   return (
-    <div>
+    <div className="bg-golf bg-no-repeat bg-fixed bg-cover">
       <div className="container mx-auto">
-        <div className="flow-root bg-green-500">
+        <div className="flow-root ">
           <p className="text-center my-10 text-yellow-300 text-4xl">SUCCESS AND CONFIRMATION</p>
 
-          <p className="text-center text-white my-10 text-2xl">
-            You've scheduled a carwash at EXAMPLETIME at EXAMPLELOCATION
+          <p className="text-center text-black font-bold my-10 text-2xl">
+            You've scheduled a teetime at EXAMPLETIME at Bushwood CC
           </p>
         </div>
       </div>
 
-      <div className="container bg-green-500">
+      <div className="container ">
         <Success />
       </div>
 
-      <div className="container flow-root bg-green-500 h-screen">
-        <p className="text-center text-white mt-10 mb-6 text-2xl">
-          Use this QR code to claim your car wash appointment in the reserved
-          line
+      <div className="container flow-root  h-screen">
+        <p className="text-center text-black font-bold mt-10 mb-6 text-2xl">
+          Use this QR code to Check In!
         </p>
         <br></br>
-        <p className="animate-bounce text-center text-white my-6 mb-10 text-2xl">
-          Refer your friends to the Suds Time App!
+        <p className="animate-bounce text-center font-bold  text-black my-6 mb-10 text-2xl">
+          Refer your friends to the Bushwood CC & Tee Time App!
         </p>
       </div>
     </div>
