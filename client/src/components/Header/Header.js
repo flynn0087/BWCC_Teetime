3c6a711cc5f3bfb5ce9303fef92a266a8573b55a
Dj Meechlouch <Meechlouch@gmail.com>
2021-03-20 02:41:59 -0500

    styling

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,8 +5,8 @@ function Input() {
   const { isLoggedIn } = useContext(LoginContext);
 
   return (
-    <div className="px-4 pb-4 text-5xl">
-      <div class="flex flex-row ...">
+    <div className="px-4 pb-4 pt-8 text-5xl">
+      <div class="text-center">
         <h1>Welcome {isLoggedIn.name}</h1>
       </div>
       <br></br>
