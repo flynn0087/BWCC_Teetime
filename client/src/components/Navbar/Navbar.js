8c2f4a2a82a0f8f4af2db6a371036cf45abe59b4
EsmondKim <ekelectronicmail@gmail.com>
2021-03-21 13:15:06 -0500

    Navbar edits and title-favicon.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
           to="/successQR"
           className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
         >
-          Confirmation
+          Useful Info
         </Link>
         <Logout />
       </div>
