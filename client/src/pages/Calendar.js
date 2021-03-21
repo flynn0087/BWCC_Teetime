e4948bc878a2e1e86ff78eed782284eb8a13a555
Kay0s <hamilton.kristina@gmail.com>
2021-03-21 10:42:01 -0500

    Calendar.js clean up

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -5,6 +5,7 @@ import Demo from "../components/FullCalendar/FullCalendar";
 import "react-calendar/dist/Calendar.css";
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
   const [value, onChange] = useState(new Date());
   let val = value.toDateString();
   return (
@@ -14,23 +15,5 @@ function App() {
   );
 }
 
-// function App() {
-//   const [value, onChange] = useState(new Date());
-
-//   return (
-//     <div>
-//       <DateTimePicker
-//         onChange={onChange}
-//         value={value}
-//         isCalendarOpen={true}
-//         format={"y-MM-dd"}
-//         // minutePlaceholder={"mm"}
-//         //hourPlaceholder={"hh"}
-//         onClick={console.log(value)}
-//         minDate={new Date()}
-//       />
-//     </div>
-//   );
-// }
 
 export default App;
