5043ab5c527909dd889a7cc6265ba917a738fd88
Kay0s <hamilton.kristina@gmail.com>
2021-03-21 10:59:36 -0500

    isAuthenticated.js clean up

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -1,7 +1,6 @@
 const User = require("../../models/User");
 module.exports = {
   ensureAuth: function (req, res, next) {
-    console.log(req.body, "HELLO FROM ENSUREAUTH!");
     // If the user is logged in, continue with the request to the restricted route
     User.findOne({ googleId: req.body.id }).then((user) => {
       if (user.isLoggedIn) {
